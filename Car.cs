dc35376c2be0a5178144d80018b86987af44ec60
Charlie Hoferer <charlie.hoferer@gmail.com>
2021-04-23 08:48:43 -0700

    Exercise 1

---
+++ Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    public class Car
+    {
+        //Create a Make property of type string that is public
+        public string Make { get; set; }
+
+        //Create a Model property of type string that is public
+
+        public string Model { get; set; }
+
+        //Create a Year property of type int that is public
+
+        public string Year { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car RangeRover = new Car();
+
+            RangeRover.Make = "Range Rover";
+            RangeRover.Model = "SVAutobiography Dynamic Black Edition";
+            RangeRover.Year = "2021";
+
+            Console.WriteLine ($"{RangeRover.Make}\n\r{RangeRover.Model}\n\r{RangeRover.Year}");
+            
+        }
+    }
+    
+
+}
